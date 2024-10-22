14cd569de513b8f2a04aad6384ae4faa5d0f2eca
Наташка Овечкина <natallia.ovechkina@gmail.com>
2024-10-22 18:11:47 +0300

    feat: add class GuessingGane

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = null;
+        this.max = null;
+        this.guessNum = null;
+    }
 
     setRange(min, max) {
-
+        this.max = max;
+        this.min = min;
     }
 
     guess() {
-
+        this.guessNum = Math.ceil((this.min + this.max) / 2);
+        return this.guessNum;
     }
 
     lower() {
-
+        this.max = this.guessNum;
     }
 
     greater() {
-
+        this.min = this.guessNum;
     }
 }
 
